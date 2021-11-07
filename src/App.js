cb709d60ee42b1b0fa8838a60507bcb298737ce1
Luu Van Tien <luuvantien2006@gmail.com>
2021-11-07 16:38:56 +0700

    add custom theme

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from "react";
 import { createBrowserHistory } from "history";
 import { Router } from "react-router-dom";
 import { renderRoutes } from "react-router-config";
+import { ThemeProvider } from "@material-ui/styles";
 
 import routes from "./routes";
 import "./mock";
+import theme from "./theme";
 const history = createBrowserHistory();
 
 function App() {
-  return <Router history={history}>{renderRoutes(routes)}</Router>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Router history={history}>{renderRoutes(routes)}</Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/theme/typography.js
@@ -0,0 +1,87 @@
+const typograpghy = {
+  textParagraph: {
+    fontSize: 14,
+    fontWeight: 500,
+    lineHeight: "24px",
+    letterSpacing: "0.003em",
+  },
+  textItem: {
+    fontSize: 16,
+    fontWeight: 500,
+    lineHeight: "24px",
+    letterSpacing: "0.003em",
+  },
+  textCategoryS: {
+    fontSize: 11,
+    fontWeight: 500,
+    lineHeight: "16px",
+    // letterSpacing: "0.003em",
+  },
+  textCategory: {
+    fontSize: 12,
+    fontWeight: 700,
+    lineHeight: "18px",
+    letterSpacing: "0.005em",
+  },
+  buttonSecondary: {
+    fontSize: 14,
+    fontWeight: 700,
+    lineHeight: "20px",
+    // letterSpacing: "0.003em",
+  },
+  buttonPrimary: {
+    fontSize: 14,
+    fontWeight: 700,
+    lineHeight: "20px",
+    // letterSpacing: "0.003em",
+  },
+  titleS: {
+    fontSize: 16,
+    fontWeight: 700,
+    lineHeight: "24px",
+    letterSpacing: "0.003em",
+  },
+  titleM: {
+    fontSize: 18,
+    fontWeight: 700,
+    lineHeight: "24px",
+    letterSpacing: "0.003em",
+  },
+  textCode: {
+    fontSize: 18,
+    fontWeight: 500,
+    lineHeight: "21.1px",
+    letterSpacing: "0.003em",
+  },
+  textStatus: {
+    fontSize: 18,
+    fontWeight: 800,
+    lineHeight: "21.1px",
+    // letterSpacing: "0.003em",
+  },
+  textItemBold: {
+    fontSize: 16,
+    fontWeight: 700,
+    lineHeight: "24px",
+    letterSpacing: "0.003em",
+  },
+  bigTitle: {
+    fontSize: 22,
+    fontWeight: 700,
+    lineHeight: "21.1px",
+    letterSpacing: "0.003em",
+  },
+  largeTitle: {
+    fontSize: 30,
+    fontWeight: 700,
+    lineHeight: "21.1px",
+    // letterSpacing: "0.003em",
+  },
+  buttonBig: {
+    fontSize: 20,
+    fontWeight: 700,
+    lineHeight: "21.1px",
+    // letterSpacing: "0.003em",
+  },
+};
+export default typograpghy;
