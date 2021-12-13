a5de730231ff860122339c6c3daf4a59479299ed
Luu Van Tien <luuvantien2006@gmail.com>
2021-12-13 16:50:56 +0700

    handle status failed

--- src/page/BillDetail/components/Status/Status.js
+++ src/page/BillDetail/components/Status/Status.js
@@ -6,52 +6,9 @@ import { Timeline } from "@material-ui/lab";
 
 function Status(props) {
   const { status } = props;
-
-  const statusList = [
-    { tag: "exported", title: "Đã xử lí xong" },
-    { tag: "delivery", title: "Đang vận chuyển" },
-    { tag: "success", title: "Giao hàng thành công" },
-    { tag: "fail", title: "Giao hàng thất bại" },
-  ];
-  const orderStaus = [
-    {
-      name: statusList[0],
-      content: "Hóa đơn xuất thành công",
-      time: moment()
-        .subtract(10, "days")
-        .subtract(0, "months")
-        .subtract(0, "years"),
-      reason: "",
-    },
-    {
-      name: statusList[1],
-      content: "Đơn hàng đang được vận chuyển",
-      time: moment()
-        .subtract(8, "days")
-        .subtract(0, "months")
-        .subtract(0, "years"),
-      reason: "",
-    },
-    {
-      name: statusList[2],
-      content: "Đơn hàng đã được vận chuyển thành công",
-      time: moment()
-        .subtract(4, "days")
-        .subtract(0, "months")
-        .subtract(0, "years"),
-      reason: "",
-    },
-    {
-      name: statusList[3],
-      content: "Vận chuyển đơn hàng thất bại",
-      time: moment()
-        .subtract(1, "days")
-        .subtract(0, "months")
-        .subtract(0, "years"),
-      reason: "Khách boom hàng",
-    },
-  ];
-
+  const numberOfFailed = status
+    ? status.filter((item) => item.name === "failed").length
+    : 0;
   return (
     <Card>
       <CardHeader title="Trạng thái" />
@@ -63,6 +20,7 @@ function Status(props) {
                   key={index}
                   item={item}
                   isLast={index === status.length - 1}
+                  numberOfFailed={numberOfFailed}
                 />
               ))
             : ""}
